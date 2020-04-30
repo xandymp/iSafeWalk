86853b5b7f0fc49a48e9928a8c0415c76641d25e
Alexandre Pereira <xandymp@gmail.com>
2020-04-30 11:20:25 -0300

    Change people CRUD title name

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 6 CRUD Example from scratch - ItSolutionStuff.com</h2>
+                <h2>People CRUD</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('people.create') }}"> Create New Person</a>
