18f6a6fa9ae1ca8d502e2c352b485138b65fe8ba
Alexandre Pereira <xandymp@gmail.com>
2020-05-06 23:36:21 -0300

    Change sidebar list - Remove use of SVG on layout blades - Change icons size

--- resources/views/people/layout.blade.php
+++ resources/views/people/layout.blade.php
@@ -18,7 +18,7 @@
                 </li>
                 <li class="active">
                     <a href="people">
-                        <i class="fa fa-user" ></i>
+                        <i class="fa fa-users" ></i>
                     </a>
                 </li>
                 <li>
@@ -29,58 +29,32 @@
             </ul>
         </nav>
         <div class="col-sm-3 title">
-            <h2>Employees&nbsp;<span style="font-size: 1.25rem;color: #cccccc">(243)</span></h2>
+            <h2>Devices&nbsp;<span style="font-size: 1.25rem;color: #cccccc">(243)</span></h2>
             <br/>
             <hr/>
             <div class="list">
                 <div class="col-sm-2">
-                    <svg class="bi bi-person" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <rect height="3em" width="3em" fill="#f1f1f1" />
-                        <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002.002zM8 7a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"/>
-                    </svg>
+                    <i class="fa fa-user-circle"></i>
                 </div>
                 <div class="col-sm-5" style="font-size: 8pt">
                     <b>Shadrias Pearson</b><br>
                     <span style="color: #999999">Warehouse Team Member</span>
                 </div>
                 <div class="col-sm-4">
-                    <svg class="bi bi-arrow-repeat" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="float: left">
-                        <path fill-rule="evenodd" d="M2.854 7.146a.5.5 0 00-.708 0l-2 2a.5.5 0 10.708.708L2.5 8.207l1.646 1.647a.5.5 0 00.708-.708l-2-2zm13-1a.5.5 0 00-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 00-.708.708l2 2a.5.5 0 00.708 0l2-2a.5.5 0 000-.708z" clip-rule="evenodd"/>
-                        <path fill-rule="evenodd" d="M8 3a4.995 4.995 0 00-4.192 2.273.5.5 0 01-.837-.546A6 6 0 0114 8a.5.5 0 01-1.001 0 5 5 0 00-5-5zM2.5 7.5A.5.5 0 013 8a5 5 0 009.192 2.727.5.5 0 11.837.546A6 6 0 012 8a.5.5 0 01.501-.5z" clip-rule="evenodd"/>
-                    </svg>
-                    &nbsp;
-                    <span style="font-size: 8pt; float: left">2 min ago</span>
-                </div>
-                <div class="col-sm-1">
-                    <svg class="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-                    </svg>
+                    <span style="font-size: 9pt; float: left">Person Status</span>
                 </div>
             </div>
             <hr/>
             <div class="list active">
                 <div class="col-sm-2">
-                    <svg class="bi bi-person" width="2em" height="2em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <rect height="3em" width="3em" fill="#f1f1f1" />
-                        <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 00.014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 00.022.004zm9.974.056v-.002.002zM8 7a2 2 0 100-4 2 2 0 000 4zm3-2a3 3 0 11-6 0 3 3 0 016 0z" clip-rule="evenodd"/>
-                    </svg>
+                    <i class="fa fa-user-circle"></i>
                 </div>
                 <div class="col-sm-5" style="font-size: 8pt">
                     <b>Beatriz Brito</b><br>
                     <span style="color: #999999">Logistics Manager</span>
                 </div>
                 <div class="col-sm-4">
-                    <svg class="bi bi-arrow-repeat" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg" style="float: left">
-                        <path fill-rule="evenodd" d="M2.854 7.146a.5.5 0 00-.708 0l-2 2a.5.5 0 10.708.708L2.5 8.207l1.646 1.647a.5.5 0 00.708-.708l-2-2zm13-1a.5.5 0 00-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 00-.708.708l2 2a.5.5 0 00.708 0l2-2a.5.5 0 000-.708z" clip-rule="evenodd"/>
-                        <path fill-rule="evenodd" d="M8 3a4.995 4.995 0 00-4.192 2.273.5.5 0 01-.837-.546A6 6 0 0114 8a.5.5 0 01-1.001 0 5 5 0 00-5-5zM2.5 7.5A.5.5 0 013 8a5 5 0 009.192 2.727.5.5 0 11.837.546A6 6 0 012 8a.5.5 0 01.501-.5z" clip-rule="evenodd"/>
-                    </svg>
-                    &nbsp;
-                    <span style="font-size: 8pt; float: left">2 min ago</span>
-                </div>
-                <div class="col-sm-1">
-                    <svg class="bi bi-check" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 010 .708l-7 7a.5.5 0 01-.708 0l-3.5-3.5a.5.5 0 11.708-.708L6.5 10.293l6.646-6.647a.5.5 0 01.708 0z" clip-rule="evenodd"/>
-                    </svg>
+                    <span style="font-size: 9pt; float: left">Person Status</span>
                 </div>
             </div>
         </div>
