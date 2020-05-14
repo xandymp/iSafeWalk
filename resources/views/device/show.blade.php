bb13c83d38fbd7b6bdbb84ac9b3c1995db495f9c
Alexandre Pereira <xandymp@gmail.com>
2020-05-14 04:52:33 -0300

    Add previous positions for device and duration at the position

--- resources/views/device/show.blade.php
+++ resources/views/device/show.blade.php
@@ -49,3 +49,60 @@
         </div>
     </div>
 </div>
+
+@if (!empty($previousLocations))
+    <hr/>
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <h4>Previous locations</h4>
+            <hr/>
+        </div>
+    </div>
+    @foreach($previousLocations as $previousLocation)
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Date:</strong>
+                    {{ date('d/m/Y - H:i:s', strtotime($previousLocation['time']))}}
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Duration:</strong>
+                    {{ $previousLocation['duration']}}
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Sector:</strong>
+                    {{ $previousLocation['sector_id'] }}
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Horizontal:</strong>
+                    {{ round($previousLocation['horizontal'], 2) }}
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Vertical:</strong>
+                    {{ round($previousLocation['vertical'], 2) }}
+                </div>
+            </div>
+        </div>
+        <hr/>
+    @endforeach
+@endif
