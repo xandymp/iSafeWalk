bf2fa703b19730d3f602c7deb5186848f241d9cf
Alexandre Pereira <xandymp@gmail.com>
2020-05-28 17:00:58 -0300

    Change layout

--- resources/views/people/interactions.blade.php
+++ resources/views/people/interactions.blade.php
@@ -13,7 +13,7 @@
 @if (!is_null($interactions))
     @foreach($interactions as $interaction)
         <div class="row">
-            <div class="col-xs-3 col-sm-3 col-md-3 primary-interaction" style="border-style: outset;">
+            <div class="col-xs-4 col-sm-4 col-md-4 primary-interaction" style="border-style: outset;">
                 <div class="row" >
                     <div class="form-group">
                         <strong>Name:</strong>
@@ -36,7 +36,7 @@
                 </div>
             </div>
             @if(!empty($interaction['secondary_interactions']))
-                <div class="col-xs-3 col-sm-3 col-md-3 secondary-interaction">
+                <div class="col-xs-4 col-sm-4 col-md-4 secondary-interaction">
                     @foreach($interaction['secondary_interactions'] as $interaction)
                         <div class="col-xs-12 col-sm-12 col-md-12" style="border-style: outset;">
                             <div class="row">
