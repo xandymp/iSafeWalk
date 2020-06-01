03004f94ffc2064eeda51a335969046521e070fd
Alexandre Pereira <xandymp@gmail.com>
2020-06-01 18:09:43 -0300

    Fix assets on deploy server

--- resources/views/gateway/layout.blade.php
+++ resources/views/gateway/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="{{ URL::to('/') }}/css/stylesheet.css" rel="stylesheet">
+    <link href="{{ asset('public') }}/css/stylesheet.css" rel="stylesheet">
 </head>
 <body>
     <div class="wrapper">
@@ -80,7 +80,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="{{ URL::to('/') }}/js/common.js"></script>
+    <script src="{{ asset('public') }}/js/common.js"></script>
 
     <script type="text/javascript" async>
         $(document).on('click', '.list', function () {
