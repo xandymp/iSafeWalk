273fb7071bac77d845750cb435b9ac1142690fae
Alexandre Pereira <xandymp@gmail.com>
2020-06-03 02:57:42 -0300

    Fix CSS and JS

--- resources/views/zone/layout.blade.php
+++ resources/views/zone/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="{{ asset('/css/stylesheet.css') }}" rel="stylesheet">
+    <link href="{{ asset('public') }}/css/stylesheet.css" rel="stylesheet">
 
 </head>
 <body>
@@ -84,7 +84,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <script src="{{ asset('/js/common.js') }}"></script>
+    <script src="{{ asset('public') }}/js/common.js"></script>
 
     <script type="text/javascript" async>
         $(document).on('click', '.list', function () {
