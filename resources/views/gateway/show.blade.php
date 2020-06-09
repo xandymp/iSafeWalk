4b6ca39b3e23dda5f50325016898d8599cd97599
Alexandre Pereira <xandymp@gmail.com>
2020-06-09 02:07:44 -0300

    Add id on item details

--- resources/views/gateway/show.blade.php
+++ resources/views/gateway/show.blade.php
@@ -9,6 +9,14 @@
 <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12">
         <div class="form-group">
+            <strong>ID:</strong>
+            {{ $gateway->id }}
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
             <strong>Name:</strong>
             {{ $gateway->name }}
         </div>
