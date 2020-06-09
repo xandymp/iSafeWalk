7519791ba9d855fabfae68d7ec552f419603fd30
Alexandre Pereira <xandymp@gmail.com>
2020-06-09 04:42:38 -0300

    Fix heat map background image

--- resources/views/people/locationMap.blade.php
+++ resources/views/people/locationMap.blade.php
@@ -93,7 +93,7 @@
                 ctx.closePath();
             @endforeach
         }
-        img.src = "/img/embalagem.png"; //transparent png
+        img.src = "{{ asset('public') }}/img/embalagem.png"; //transparent png
 
     });
 </script>
